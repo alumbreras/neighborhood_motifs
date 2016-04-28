129394677c97711e35ce2f782de59f2239bbd39b
Alberto Lumbreras <alberto.lumbreras@gmail.com>
2016-04-28 12:14:34 +0200

    backup

--- R/plotting.r
+++ R/plotting.r
@@ -18,3 +18,24 @@ plot.tree <- function(gtree){
        vertex.size=1,
        edge.arrow.size=0.6)
 }
+
+plot.trees <- function(trees, labels){
+  # Plots a set of trees in a grid
+  # Arguments:
+  #   trees: a list of igraph tree objects
+  #   labels: a label for each tree.
+  mypalette <- c("black", "yellow", "orange", "red", "white")
+  par(mfrow=c(3,5))
+  
+  for(i in 1:length(trees)){
+    gmotif <- as.undirected(trees[[i]])
+    la = layout_as_tree(gmotif, mode='out', root=which.min(V(gmotif)$date))
+    plot(gmotif,
+         layout = la,
+         vertex.color=mypalette[V(gmotif)$color],
+         vertex.label = "",
+         edge.arrow.size=0.6)
+    title(labels[i])
+  }
+  
+}
